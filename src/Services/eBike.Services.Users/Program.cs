a7ff739e00a99331ed7c0b76b471d7dfacf115f0
Andrea Tosato <andrea.tosato@hotmail.it>
2021-06-18 18:53:56 +0200

    Update API

--- src/Services/eBike.Services.Users/Program.cs
+++ src/Services/eBike.Services.Users/Program.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Text.Json;
-using System.Threading.Tasks;
 using Dapr.Client;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MongoDB.Driver;
@@ -18,33 +15,16 @@ if (app.Environment.IsDevelopment()) {
     app.UseDeveloperExceptionPage();
 }
 
-app.MapGet("/{id}", async (http) => {
-    if (!http.Request.RouteValues.TryGetValue("id", out var id)) {
-        http.Response.StatusCode = StatusCodes.Status400BadRequest;
-        return;
-    }
+app.MapGet("/{id}", (int id, DaprClient dapr) => dapr.GetStateAsync<User>("user-state", $"user-{id}"));
 
-    var dapr = http.RequestServices.GetRequiredService<DaprClient>();
-    await http.Response.WriteAsJsonAsync<User>(
-        await dapr.GetStateAsync<User>("user-state", $"user-{id}")
-    );
-});
-
-app.MapPost("/v1/User", async (http) => 
+app.MapPost("/v1/User", async (User newUser, DaprClient dapr) => 
 {
-    var newUser = await JsonSerializer.DeserializeAsync<User>(http.Request.Body);
-    var dapr = http.RequestServices.GetRequiredService<DaprClient>();
-
     await GetUserCollection().FindOneAndReplaceAsync(t => t.Id == newUser.Id, newUser);
-
     await dapr.PublishEventAsync("pubsub", "user-insert", newUser);
 });
 
-app.MapPost("/v1/UserInsert", async (http) => {
-    var newUser = await JsonSerializer.DeserializeAsync<User>(http.Request.Body);
-    var dapr = http.RequestServices.GetRequiredService<DaprClient>();
-    await dapr.SaveStateAsync<User>("user-state", $"user-{newUser.Id}", newUser);
-}).WithTopic("pubsub", "user-insert");
+app.MapPost("/v1/UserInsert", (User newUser, DaprClient dapr) => dapr.SaveStateAsync<User>("user-state", $"user-{newUser.Id}", newUser))
+   .WithTopic("pubsub", "user-insert");
 
 await app.RunAsync();
 
